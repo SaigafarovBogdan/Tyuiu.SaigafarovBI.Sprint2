7bba7f2e0cf1b3509261e50820d9595c1dca836e
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-13 20:19:10 +0500

    Task6

--- Tyuiu.SaigafarovBI.Sprint2.Task6.V11.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint2.Task6.V11.Lib/DataService.cs
@@ -6,29 +6,123 @@ namespace Tyuiu.SaigafarovBI.Sprint2.Task6.V11.Lib
         public string FindDateOfNextDay(int g, int m, int n)
         {
             string result = "";
-            string day = GetDay(n);
-            string month = GetMonth(m);
-            if (GetDay(n) == "32")
+            string month = "";
+            string day = "";
+            switch (m)
             {
-                month = Convert.ToString(m + 1);
-                day = "01";
+                case 1:
+                    {
+                        month = "01";
+                        break;
+                    }
+                case 2:
+                    {
+                        month = "02";
+                        break;
+                    }
+                case 3:
+                    {
+                        month = "03";
+                        break;
+                    }
+                case 4:
+                    {
+                        month = "04";
+                        break;
+                    }
+                case 5:
+                    {
+                        month = "05";
+                        break;
+                    }
+                case 6:
+                    {
+                        month = "06";
+                        break;
+                    }
+                case 7:
+                    {
+                        month = "07";
+                        break;
+                    }
+                case 8:
+                    {
+                        month = "08";
+                        break;
+                    }
+                case 9:
+                    {
+                        month = "09";
+                        break;
+                    }
+                case 10:
+                    {
+                        month = "10";
+                        break;
+                    }
+                case 11:
+                    {
+                        month = "11";
+                        break;
+                    }
+                case 12:
+                    {
+                        month = "12";
+                        break;
+                    }
+                default:
+                    {
+                        throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {m}");
+                    }
+
             }
-            result = day + "." + month + "." + Convert.ToString(g);
-            return result;
-        }
-        private string GetDay(int n)
-        {
             switch (n+1)
             {
-                case 1: return "01";
-                case 2: return "02";
-                case 3: return "03";
-                case 4: return "04";
-                case 5: return "05";
-                case 6: return "06";
-                case 7: return "07";
-                case 8: return "08";
-                case 9: return "09";
+                case 1:
+                    {
+                        day = "02";
+                        break;
+                    }
+                case 2:
+                    {
+                        day = "03";
+                        break;
+                    }
+                case 3:
+                    {
+                        day = "04";
+                        break;
+                    }
+                case 4:
+                    {
+                        day = "05";
+                        break;
+                    }
+                case 5:
+                    {
+                        day = "06";
+                        break;
+                    }
+                case 6:
+                    {
+                        day = "07";
+                        break;
+                    }
+                case 7:
+                    {
+                        day = "08";
+                        break;
+                    }
+                case 8:
+                    {
+                        day = "09";
+                        break;
+                    }
+                case 9:
+                    {
+                        day = "10";
+                        break;
+                    }
                 case 10:
                 case 11:
                 case 12:
@@ -50,34 +144,29 @@ namespace Tyuiu.SaigafarovBI.Sprint2.Task6.V11.Lib
                 case 28:
                 case 29:
                 case 30:
-                case 31: return Convert.ToString(n);
-                case 32: return "32";
+                case 31:
+                    {
+                        day = Convert.ToString(n + 1);
+                        break;
+                    }
+                case 32:
+                    {
+                        day = "32";
+                        break;
+                    }
                 default:
                     {
                         throw new ArgumentException($"День должен быть от 1 до 31. Значение {n}");
                     }
 
             }
-        }
-        private string GetMonth(int m)
-        {
-            switch (m)
+            if (day == "32")
             {
-                case 1: return "01";
-                case 2: return "02";
-                case 3: return "03";
-                case 4: return "04";
-                case 5: return "05";
-                case 6: return "06";
-                case 7: return "07";
-                case 8: return "08";
-                case 9: return "09";
-                case 10: return "10";
-                case 11: return "11";
-                case 12: return "12";
-                default: throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {m}");
-
+                month = Convert.ToString(m + 1);
+                day = "01";
             }
+            result = day + "." + month + "." + Convert.ToString(g);
+            return result;
         }
     }
 }
