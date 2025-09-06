acbb224bb9e51f733724fd247c54a618d486de4b
SaigafarovBogdan <sajgafarovb@gmail.com>
2025-09-06 20:50:43 +0500

    Test NuGet

--- Tyuiu.SaigafarovBI.Sprint2.Task2.V7.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint2.Task2.V7.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
+﻿using tyuiu.courses.programming.interfaces.Sprint2;
+
 namespace Tyuiu.SaigafarovBI.Sprint2.Task2.V7.Lib
 {
     public class DataService : ISprint2Task2V7
