99cce20f0b430e3c7061c0524472fc657130c9b7
SaigafarovBogdan <sajgafarovb@gmail.com>
2026-02-14 10:16:50 +0500

    Update DataService.cs

--- Tyuiu.SaigafarovBI.Sprint2.Task7.V6.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint2.Task7.V6.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.SaigafarovBI.Sprint2.Task7.V6.Lib
             if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 4) && ((x > 0) && (x <= 2) && ((y > 0) && (x <= 2))))
             {
                 if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x > 0) && (x <= 1) && ((y > 0) && (x <= 1)))) return false;
-                else return true;
+                else return false;
             }
             else return true;
         }
